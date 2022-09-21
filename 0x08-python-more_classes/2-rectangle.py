f5831e471b41a57e0ace3ea7339bc81be7f602d1
Esther-Karanja <est.karanja@gmail.com>
2022-09-21 12:26:33 -0700

    fix2 correct output

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -37,10 +37,16 @@ class Rectangle:
 
     def area(self):
         """returns the area of the rectangle"""
-        return self.__width * self.__height
+        return self.width * self.length
 
     def perimeter(self):
         """returns the perimeter of the rectangle"""
         if self.__width is 0 or self.__height is 0:
             return 0
         return (self.width * 2) + (self.lengith * 2)
+
+    print("my_rectangle")
+    for i in range(width):
+        for j in range(lenth):
+            print('#', end=' ')
+            print()
